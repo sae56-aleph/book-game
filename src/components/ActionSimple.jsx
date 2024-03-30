e0fd9157b64a95b7023e6172a055a189bb2d0701
Enzo MAROS <enzo.maros@gmail.com>
2024-03-30 17:19:13 +0100

    Amélioration style App & ActionSimple

--- src/components/ActionSimple.jsx
+++ src/components/ActionSimple.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 const ActionSimple = ({ target, text, onClick }) => {
-  const handleClick = () => {
-    onClick(target);
-  };
+    const handleClick = () => {
+        onClick(target);
+    };
 
-  return (
-    <button style={{ color: '#461C89' }} onClick={handleClick}>
-      {text}
-    </button>
-  );
+    return (
+        <button
+            style={{
+                color: "#461C89",
+                background: "none",
+                border: "none",
+                cursor: "pointer",
+                fontWeight: "bold",
+                fontSize: "20px",
+            }}
+            onClick={handleClick}
+        >
+            {text}
+        </button>
+    );
 };
 
 ActionSimple.propTypes = {
-  target: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
+    target: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
 };
 
 export default ActionSimple;
