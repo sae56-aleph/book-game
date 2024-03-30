c867f98c5e5ce8d6d7c9c0c08a4d5001c9fb21c9
SimonnCode <simonfouchet1712@gmail.com>
2024-03-30 16:50:51 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import './App.css'
 import Title from './components/Titre'
 import Bouton from './components/Bouton'
 import Image from './components/Image'
+import ActionSimple from './components/ActionSimple'
 
 function App() {
 
@@ -12,8 +10,9 @@ function App() {
     <>
       <div>
         <Title level={1} text="je test"></Title>
-        <Bouton text="{}" isDisabled={true}></Bouton>
-        <Image url="https://cdn.futura-sciences.com/cdn-cgi/image/width=1024,quality=50,format=auto/sources/images/dossier/773/01-intro-773.jpg" height={0} width={0}></Image>
+        <Bouton text="{}" isDisabled={false}></Bouton>
+        <Image url="https://cdn.futura-sciences.com/cdn-cgi/image/width=1024,quality=50,format=auto/sources/images/dossier/773/01-intro-773.jpg" height={100} width={100}></Image>
+        <ActionSimple target="https://example.com" text="Cliquez ici" onClick={console.log("test")}></ActionSimple>
       </div>
     </>
   )
