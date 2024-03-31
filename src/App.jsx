a9a49d3b7180c69722f75e415ee540804339dffa
AlexieG <alexie.grosbois2002@gmail.com>
2024-03-31 19:29:54 +0200

    Ajout du composant bloc et de son style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import Title from "./components/Titre";
+import Titre from "./components/Titre";
 import Bouton from "./components/Bouton";
 import Image from "./components/Image";
 import ActionSimple from "./components/ActionSimple";
+import Bloc from "./components/Bloc";
 
 function App() {
   return (
     <>
       <div>
-        <Title level={1} text="je test" />
+        <Titre level={1} text="je test" />
         <Bouton text="{}" />
         <Image
           url="https://cdn.futura-sciences.com/cdn-cgi/image/width=1024,quality=50,format=auto/sources/images/dossier/773/01-intro-773.jpg"
@@ -19,6 +20,9 @@ function App() {
           text="Cliquez ici"
           onClick={console.log("test")}
         />
+        <Bloc>
+          <p>Enzooooo</p>
+        </Bloc>
       </div>
     </>
   );
