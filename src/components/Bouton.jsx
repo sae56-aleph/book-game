3372d350e5a7b3bc8d5034281078aee3404688b1
Enzo MAROS <enzo.maros@gmail.com>
2024-03-31 18:08:22 +0200

    Icone droite et gauche Bouton

--- src/components/Bouton.jsx
+++ src/components/Bouton.jsx
@@ -2,19 +2,31 @@ import styles from "./Bouton.module.css";
 import React from "react";
 import PropTypes from "prop-types";
 
-const Bouton = ({ text, onClick, isDisabled = false }) => {
+const Bouton = ({
+  text,
+  onClick,
+  icon,
+  iconPosition = "left",
+  isDisabled = false,
+}) => {
+  const Icon = icon;
+
   return (
     <button
       className={`${styles.bouton} ${isDisabled && styles.disabled}`}
       onClick={onClick}
       disabled={isDisabled}
     >
+      {icon && iconPosition === "left" && <Icon height={18} width={18} />}
       {text}
+      {icon && iconPosition === "right" && <Icon height={18} width={18} />}
     </button>
   );
 };
 
 Bouton.propTypes = {
+  icon: PropTypes.any,
+  iconPosition: PropTypes.oneOf(["left", "right"]),
   onClick: PropTypes.func,
   text: PropTypes.string.isRequired,
   isDisabled: PropTypes.bool,
