74d2828516ca262aaa33c0b1ea1b9a2949d50108
Enzo MAROS <enzo.maros@gmail.com>
2024-04-05 11:51:24 +0200

    TTS

--- src/components/SpeechBouton.jsx
+++ src/components/SpeechBouton.jsx
@@ -1,23 +1,67 @@
 import styles from "./Bouton.module.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import MegaphoneFill from "../icons/megaphone-fill.svg?react";
+import PauseLine from "../icons/pause-line.svg?react";
 
 /**
  * Speech Bouton
  * @author Simon FOUCHET
  */
-const SpeechBouton = ({ icon, onClick }) => {
-  const Icon = icon;
+const SpeechBouton = ({ chapterId }) => {
+  const [audio, setAudio] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    if (audio !== null) {
+      audio.pause();
+      setIsPlaying(false);
+    }
+
+    const url = new URL(
+      "section/" + chapterId + "/audio",
+      import.meta.env.VITE_API_URL
+    );
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        const audio = new Audio("/audio/" + data.audio);
+        audio.addEventListener("ended", () => setIsPlaying(false));
+
+        setAudio(audio);
+      });
+  }, [chapterId]);
+
+  const handleClick = () => {
+    if (audio === null) return;
+
+    if (isPlaying) {
+      audio.pause();
+      setIsPlaying(false);
+    } else {
+      audio.play();
+      setIsPlaying(true);
+    }
+  };
+
   return (
-    <button className={`${styles.bouton}`} onClick={onClick}>
-      {icon && <Icon height={18} width={18} />}
+    <button
+      className={`${styles.bouton} ${audio === null ? styles.disabled : ""}`}
+      onClick={handleClick}
+      style={{ marginBottom: 14 }}
+    >
+      {isPlaying ? (
+        <PauseLine height={18} width={18} />
+      ) : (
+        <MegaphoneFill height={18} width={18} />
+      )}
     </button>
   );
 };
 
 SpeechBouton.propTypes = {
-  onClick: PropTypes.func,
-  icon: PropTypes.any,
+  chapterId: PropTypes.string.isRequired,
 };
 
 export default SpeechBouton;
