a2ec6a838ac7c6f7a8909183608f9e2e373718a7
SimonnCode <simonfouchet1712@gmail.com>
2024-04-05 08:34:20 +0200

    bouton done

--- src/components/SpeechBouton.jsx
+++ src/components/SpeechBouton.jsx
@@ -1,22 +1,23 @@
 import styles from "./Bouton.module.css";
 import React from "react";
 import PropTypes from "prop-types";
-// import MegaPhone from "../../../icons/megaphone-fill.svg?react";
 
 /**
- * Bouton
+ * Speech Bouton
  * @author Simon FOUCHET
  */
-const SpeechBouton = ({ onClick }) => {
+const SpeechBouton = ({ icon, onClick }) => {
+  const Icon = icon;
   return (
     <button className={`${styles.bouton}`} onClick={onClick}>
-      {/* <MegaPhone /> */}
+      {icon && <Icon height={18} width={18} />}
     </button>
   );
 };
 
 SpeechBouton.propTypes = {
   onClick: PropTypes.func,
+  icon: PropTypes.any,
 };
 
 export default SpeechBouton;
